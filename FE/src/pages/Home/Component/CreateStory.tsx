ed4cadcdffb8a6320e53666aea8d20c13bcaf09f
thanhdo21d <114094731+thanvandung1204@users.noreply.github.com>
2024-06-21 21:07:42 +0700

    updated

--- FE/src/pages/Home/Component/CreateStory.tsx
+++ FE/src/pages/Home/Component/CreateStory.tsx
@@ -1,44 +1,90 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { IonIcon } from '@ionic/react'
 import StoryApi from '~/apis/story.api'
 import axios from 'axios'
+import useMutationLogin from './hook/useMutationCreate'
+import { toast } from 'react-toastify'
+import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom'
+import useMutationEdit from './hook/useEditMutation'
 
-function CreateStory({handelCheckToggle}:any) {
+function CreateStory({ handelCheckToggle }: any) {
   const [dataStory, setDataStory] = useState('')
   const [fileName, setFileName] = useState('')
   const [file, setFile] = useState<any>(null)
   const dataFileok = localStorage.getItem('fileName')
-   const auth = localStorage.getItem('access_token')
+  const auth = localStorage.getItem('access_token')
   const closeCreate = () => {
+    localStorage.removeItem('fileName')
     handelCheckToggle(false)
   }
-  const createStory = async () => {
-    try {
-      // Assuming axios is imported correctly
-      console.log('click')
-      const test = {
-        content: dataFileok,
-        text: dataStory,
-        privacy: '1',
-        tag: '1231323',
-        music: 'ccc'
+  const [searchParams] = useSearchParams()
+  const idSearch: any = searchParams.get('isEdit')
+  const idSearch2: any = searchParams.get('id')
+  const navigate = useNavigate()
+  const mutation = useMutationLogin()
+  const mutaionEdit = useMutationEdit()
+  const handelCreate = (data: any) => {
+    mutation.mutate(data, {
+      onSuccess: () => {
+        toast.success('Story created successfully')
+        closeCreate()
+      },
+      onError: (error) => {
+        console.log('Error creating story:', error)
       }
-      await axios.post('http://localhost:3000/api/v1/story/add', test, {
-        headers: {
-          Authorization: auth // Assuming token is defined elsewhere
-        }
-      })
-    } catch (error) {
-      console.error('Error creating story:', error)
-    }
+    })
   }
+  const handelEdit = (id: any, data: any) => {
+    mutaionEdit.mutate(
+      { id, data },
+      {
+        onSuccess: () => {
+          toast.success('Story edit successfully')
+          window.location.reload()
+        },
+        onError: (error) => {
+          console.log('Error creating story:', error)
+        }
+      }
+    )
+  }
+  const createStory = () => {
+    const test = {
+      content: dataFileok,
+      text: dataStory,
+      privacy: '1',
+      tag: '1231323',
+      music: 'ccc'
+    }
+    idSearch ? handelEdit(idSearch2, test) : handelCreate(test)
+  }
+  const reload2 = () => {
+    localStorage.removeItem('fileName')
+
+    navigate({
+      search: createSearchParams({
+        id: ''
+      }).toString()
+    })
+    window.location.reload()
+  }
+  useEffect(() => {
+      setFileName(dataFileok)
+  },[dataFileok])
+  console.log(fileName,'ok')
   return (
     <div className='absolute '>
       <div className='uk-modal-dialog tt dark:bg-dark2 relative mx-auto w-full overflow-hidden rounded-lg bg-white p-7 shadow-xl md:w-[520px]'>
         <div className='-m-7 mb-0 border-b py-3 text-center dark:border-slate-700'>
-          <h2 className='text-sm font-medium'> Tạo Trạng Thái </h2>
+          <h2 className='text-sm font-medium'> {idSearch ? 'Sửa Trạng Thái' : 'Tạo Trạng Thái'} </h2>
           {/* nút đóng */}
-          <button onClick={closeCreate} type='button' className='button__ico uk-modal-close absolute right-0 top-0 m-2.5'>
+          <button
+            onClick={() => {
+              idSearch ? reload2() : closeCreate()
+            }}
+            type='button'
+            className='button__ico uk-modal-close absolute right-0 top-0 m-2.5'
+          >
             <svg
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
@@ -59,34 +105,42 @@ function CreateStory({handelCheckToggle}:any) {
             <input onChange={(e) => setDataStory(e.target.value)} type='text' className='mt-3 w-full' />
           </div>
           <div>
-            <div className="border1 relative h-72 w-full overflow-hidden rounded-lg bg-[url('../images/ad_pattern.png')] bg-repeat">
+            <div
+              style={{
+                backgroundImage: `url(${'http://localhost:3000/' + fileName})`
+              }}
+              className='border1 relative h-72 w-full overflow-hidden rounded-lg  bg-repeat'
+            >
               <label
                 htmlFor='createStatusUrl'
                 className='absolute bottom-0 left-1/2 z-10 flex w-full -translate-x-1/2 cursor-pointer flex-col items-center justify-center bg-gradient-to-t from-gray-700/60 pb-6 pt-10'
               >
                 <input
-                  onChange={ (event: any) => {
+                  onChange={(event: any) => {
+                    localStorage.removeItem('fileName')
                     const formData = new FormData()
                     console.log(event.target.files[0])
-                      formData.append('avatar', event.target.files[0])
-                      try {
-                          axios.post(`http://localhost:3000/upload-avatar`, formData, {
+                    formData.append('avatar', event.target.files[0])
+                    try {
+                      axios
+                        .post(`http://localhost:3000/upload-avatar`, formData, {
                           headers: {
                             'Content-Type': 'multipart/form-data'
                           }
-                          }).then((respo) => {
-                            const { data } = respo
-                            if (data) {
-                              console.log(data, 'ccc')
-                              localStorage.setItem('fileName', data.avatarFileName)
-                          setFileName(data.avatarFileName)
-                        }
                         })
+                        .then((respo) => {
+                          const { data } = respo
+                          if (data) {
+                            console.log(data, 'ccc')
+                            localStorage.setItem('fileName', data.avatarFileName)
+                            setFileName(data.avatarFileName)
+        toast.success('upload image successfully')
 
-                      } catch (error) {
-                        console.error(error)
-                      }
-
+                          }
+                        })
+                    } catch (error) {
+                      console.error(error)
+                    }
                   }}
                   id='createStatusUrl'
                   type='file'
@@ -112,13 +166,11 @@ function CreateStory({handelCheckToggle}:any) {
                 className='rounded-full bg-blue-50 text-3xl text-sky-600 dark:bg-transparent'
               />
               <p className='text-sm font-medium text-gray-500'>
-                {' '}
                 Trạng thái của bạn sẽ có sẵn <br /> trong <span className='text-gray-800'> 24 giờ</span>{' '}
               </p>
             </div>
             <button onClick={createStory} type='button' className='button relative z-50 bg-blue-500 px-8 text-white'>
-              {' '}
-              Tạo
+              {idSearch ? 'Sửa ' : 'Tạo '}
             </button>
           </div>
         </form>
--- FE/src/pages/Home/Component/ListStory.tsx
+++ FE/src/pages/Home/Component/ListStory.tsx
@@ -3,12 +3,21 @@ import { Story } from '~/@types/story'
 import storyApi from '~/apis/story.api'
 import axios, { AxiosResponse } from 'axios'
 import { IonIcon } from '@ionic/react'
-import { createSearchParams, useNavigate } from 'react-router-dom'
+import { createSearchParams, useNavigate, useSearchParams } from 'react-router-dom'
+import useMutationRemove from './hook/useMutationRemove'
+import { toast } from 'react-toastify'
+import CreateStory from './CreateStory'
 
 function ListStory({ handelCheckToggle }: any) {
   const toggleStory = () => {
     handelCheckToggle(true)
   }
+  const [searchParams] = useSearchParams()
+  const [show, setShow] = useState(false)
+  const [showEdit, setShowEdit] = useState(false)
+
+  const idSearch: any = searchParams.get('id')
+  // http://localhost:3000/api/v1/storydelete/:storyId
   const navigate = useNavigate()
   const [dataStory, setDataStory] = useState([])
   const auth = localStorage.getItem('access_token')
@@ -24,58 +33,112 @@ function ListStory({ handelCheckToggle }: any) {
   useEffect(() => {
     getStoryuser()
   }, [])
-    const elements = document.querySelectorAll('.uk-lightbox.uk-overflow-hidden.uk-lightbox-panel.uk-open');
+  const button: any = document.querySelector('.uk-lightbox-toolbar-icon.uk-close-large.uk-icon.uk-close')
 
-useEffect(() => {
-  setInterval(() => {
-      console.log(elements,'elements');
-    },2000)
-  }, [elements]);
+  button?.addEventListener('click', () => {
+    setShow(false)
+    setShowEdit(false)
+    navigate({
+      search: createSearchParams({
+        id: ''
+      }).toString()
+    })
+  })
+  const mutation = useMutationRemove()
+  const handelCreate = (data: any) => {
+    mutation.mutate(data, {
+      onSuccess: () => {
+        toast.success('Story remove successfully')
+        setTimeout(() => {
+          window.location.reload()
+        }, 450)
+      },
+      onError: (error) => {
+        console.log('Error creating story:', error)
+      }
+    })
+  }
   return (
     <>
-    <div className='flex  gap-2'>
-      <li onClick={toggleStory} className='md:pr-3' uk-scrollspy-class='uk-animation-fade'>
-        <div className='dark:bg-dark2 relative grid h-12 w-12 shrink-0 place-items-center rounded-full border-2 border-dashed border-slate-300 bg-slate-200 md:h-16 md:w-16 dark:border-slate-700'>
-          <IonIcon icon='camera' className='text-2xl' />
+      {show && (
+        <>
+          <button
+            onClick={() => handelCreate(idSearch)}
+            style={{ zIndex: '9999999999999999999999999999999999999999999' }}
+            className='absolute right-0 top-0 bg-red-500 text-white'
+          >
+            xoá
+          </button>
+          <button
+            onClick={() => {
+              setShowEdit(true)
+              setShow(false)
+              navigate({
+                search: createSearchParams({
+                  id: idSearch,
+                  isEdit: '1'
+                }).toString()
+              })
+            }}
+            style={{ zIndex: '9999999999999999999999999999999999999999999' }}
+            className='absolute right-0 top-10 bg-red-500 text-white'
+          >
+            sửa
+          </button>
+        </>
+      )}
+      {showEdit ? (
+        <div
+          style={{ zIndex: '99999999999999999999999999999999999999999999999999' }}
+          className='absolute bottom-0 left-0 right-0 top-0'
+        >
+          <CreateStory />
         </div>
-      </li>
-      <div className=' flex items-center gap-3'>
-        {dataStory?.length > 1 ? (
-          dataStory.map((items: any) => {
-            return (
-              <ul
-                onClick={() =>
-                  navigate({
-                    search: createSearchParams({
-                      id: items.story_id
-                    }).toString()
-                  })
-                }
-                key={items.story_id}
-                className='uk-slider-items w-[calc(100%+14px)]'
-                uk-scrollspy='target: > li; cls: uk-animation-scale-up; delay: 20; repeat: true'
-              >
-                <li key='' className='pr-2 duration-300 hover:-rotate-2 hover:scale-[1.15] md:pr-3'>
-                  <a href={'http://localhost:3000/' + items.content} data-caption={items.text}>
-                    <div className='relative h-12 w-12 overflow-hidden rounded-full border-2 border-white shadow md:h-16 md:w-16 md:border-4 dark:border-slate-700'>
-                      <img
-                        src={'http://localhost:3000/' + items.content}
-                        alt=''
-                        className='absolute object-cover'
-                      />
-
-                    </div>
-                  </a>
-                </li>
-              </ul>
-            )
-          })
-        ) : (
-          <></>
-        )}
-      </div>
-      </div>
-      </>
+      ) : (
+        <div className='flex  gap-2'>
+          <p onClick={toggleStory} className='md:pr-3' uk-scrollspy-class='uk-animation-fade'>
+            <div className='dark:bg-dark2 relative grid h-12 w-12 shrink-0 place-items-center rounded-full border-2 border-dashed border-slate-300 bg-slate-200 md:h-16 md:w-16 dark:border-slate-700'>
+              <IonIcon icon='camera' className='text-2xl' />
+            </div>
+          </p>
+          <div className=' flex items-center gap-3'>
+            {dataStory?.length > 1 ? (
+              dataStory.map((items: any) => {
+                return (
+                  <ul
+                    onClick={() => {
+                      setShow(true)
+                      navigate({
+                        search: createSearchParams({
+                          id: items.story_id
+                        }).toString()
+                      })
+                    }}
+                    key={items.story_id}
+                    className='uk-slider-items w-[calc(100%+14px)]'
+                    uk-scrollspy='target: > li; cls: uk-animation-scale-up; delay: 20; repeat: true'
+                  >
+                    <li key='' className='pr-2 duration-300 hover:-rotate-2 hover:scale-[1.15] md:pr-3'>
+                      <a href={'http://localhost:3000/' + items.content} data-caption={items.text}>
+                        <div className='relative h-12 w-12 overflow-hidden rounded-full border-2 border-white shadow md:h-16 md:w-16 md:border-4 dark:border-slate-700'>
+                          <img
+                            src={'http://localhost:3000/' + items.content}
+                            alt=''
+                            className='absolute object-cover'
+                          />
+                        </div>
+                      </a>
+                    </li>
+                  </ul>
+                )
+              })
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
